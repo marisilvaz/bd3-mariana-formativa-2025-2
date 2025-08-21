8ecbd27f8b9e90ceea631e88e53fe1fe8cd5f643
Mariana Silva <138073195+marisilvaz@users.noreply.github.com>
2025-08-20 22:19:25 -0300

    BDIII: Semana03 | selects-ops

---
+++ bd3-mariana-formativa-2025-2/semana02/update-delete.mongodb.js
@@ -0,0 +1,18 @@
+/* CRIAÇÃO DA CONSTANTE QUE REPRESENTA O NOME DO BANCO DE DADOS */
+const database = 'BD3-Projeto';
+
+/* CRIAÇÃO DA CONSTANTE QUE REPRESENTA O NOME DA COLEÇÃO DE ARQUIVOS */
+const collection = 'Livraria';
+
+/* DEFINE O BANCO DE DADOS QUE SERÁ UTILIZADO, CASO NÃO EXISTA, SERÁ CRIADO */
+use(database);
+
+/* ATUALIZAÇÃO DE UM ARQUIVO NO MONGODB: */
+/*db['Livraria'].update(
+    {codigo: '1'},
+    {$set:{titulo:'TESTE DE ALTERAÇÃO DE TÍTULO'}}
+);
+*/
+
+/* EXCLUIR UM ARQUIVO NO MONGODB: */
+db["Livraria"].deleteOne({codigo:'1'});
